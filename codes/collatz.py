d61d2edc3f9d7477e668f1beadc181b8caa871e3
abdulaziz <abdulaziz@localhost.localdomain>
2023-10-11 08:19:59 +0300

    Addition of new code

---
+++ codes/collatz.py
@@ -0,0 +1,34 @@
+def collatz(n):
+    if n%2 == 0:
+        return int(n/2)
+    else:
+        return 3*n+1
+
+def program(_input):
+    string = ""
+    num = _input
+    count = 1
+    while num > 1:
+        string += str(num) + " -> "
+        num = collatz(num)
+        count += 1
+    string += "1"
+    return (string, count)
+
+from js import document
+from pyodide.ffi import create_proxy
+
+@create_proxy
+def click(*args):
+    _input = document.getElementById('initial_num').value
+    if _input.isdigit():
+        initial_num = int(_input)
+        output = program(initial_num)[0]
+        length = "Length = " + str(program(initial_num)[1])
+    else:
+        output = "Invalid Entry."
+    document.getElementById('output').innerText = output
+    document.getElementById('len').innerText = length
+
+document.getElementById('click').addEventListener('click',click)
+
